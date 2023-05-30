ae0e80c773e0adb3cf4124c9173bf51fc2f07938
10cheon00 <10cheon00@gmail.com>
2023-05-30 13:24:35 +0900

    remove: src/

--- src/default-scene/scene.js
+++
@@ -1,7 +0,0 @@
-import { GameObject } from "/src/engine/module.js";
-
-export default class ExampleScene extends GameObject {
-  constructor() {
-    super();
-  }
-}
--- src/engine/utils.js
+++
@@ -1,4 +0,0 @@
-import Timer from "/src/engine/utils/timer.js";
-
-export * from "/src/engine/utils/helper.js";
-export { Timer };
